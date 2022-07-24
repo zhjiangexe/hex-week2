2891a7f34193ec684869e9751e669bf4d777f9cf
jiang <jiang.jiang@eton.tw>
2022-07-25 00:10:23 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './reset.css'
+import './index.sass'
+
+// function component () {
+//   const element = document.createElement('div');
+//   element.classList.add('hello');
+//   return element;
+// }
+
+// document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ghpages = require('gh-pages');
+
+ghpages.publish('dist', function(err) {});
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'images/[name][ext]'
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    // compress: true,
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules:[
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          }
+        ],
+      },
+      {
+        test: /\.(s[ac]ss|css)$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'one',
+      template: './src/index.html'
+    }),
+  ]
+};
